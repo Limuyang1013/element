4b1a0e6d17c38b71716ab69cbd2a8cb4c6cebda2
LiMuYang <Lin794653318@gmail.com>
2017-09-17 21:53:07 +0800

    适配header布局
    适配header布局

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
@@ -22,6 +17,9 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi':['error','never'],
+    'indent':0,
+    'space-before-function-paren': 0
   }
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
   	<div class="header">
-  		header
+  		<v-header></v-header>
   	</div>
   	<div class="tab">
-  		tab
+  		<div class="tab-item">商品</div>
+  		<div class="tab-item">评论</div>
+  		<div class="tab-item">商家</div>
   	</div>
   	<div class="content">
   		content
@@ -17,20 +19,22 @@
 <script>
 import header from './components/header/header.vue'
 export default {
-  componets: {
-    header
+  components: {
+    'v-header': header
   },
   name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+ .tab
+   display: flex
+   width: 100%
+   height: 40px
+   line-height: 40px
+   .tab-item
+     flex: 1
+     text-align: center
+
 </style>
