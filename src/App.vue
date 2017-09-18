da430b41853006205f22ce2fbff4f3871381e2d5
Limuyang <lin794653318@gmail.com>
2017-09-18 18:16:58 +0800

    请求seller数据

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,27 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 import header from './components/header/header.vue'
 import './common/stylus/index.styl'
+const ERR_OK = 0
 export default {
+  data() {
+    return {
+      seller: {
+
+      }
+    }
+  },
+  created() {
+    this.$http.get('./api/seller').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.seller = response.data
+        console.log(this.seller)
+      }
+    })
+  },
   components: {
     'v-header': header
   },
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
+Vue.use(router)
+Vue.use(VueResource)
 /* 关闭生产模式下给出的提示 */
 Vue.config.productionTip = false
 
