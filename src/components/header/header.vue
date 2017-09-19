900a3741720bee3b9df37633b3b39c4cd67f59f5
LiMuYang <Lin794653318@gmail.com>
2017-09-19 23:05:55 +0800

    header组件开发完毕
    header组件开发完毕

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -22,7 +22,14 @@
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span
+      class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
 	</div>
 </template>
 
@@ -42,8 +49,9 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .header
+    position: relative
     color: #fff
-    background: #999
+    background: rgba(7,17,27,0.5)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -104,7 +112,7 @@
         position: absolute
         right: 12px
         bottom: 14px
-        padding: 8px
+        padding: 0 8px
         height: 24px
         line-height: 24px
         border-radius: 14px
@@ -119,4 +127,41 @@
           font-size: 10px
 
 
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        width: 22px
+        height: 12px
+        margin-top: 7px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+        margin: 0 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+
+
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
