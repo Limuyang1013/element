86e80616b6d0548fd07efb52ffb434578d0ddb62
Limuyang <lin794653318@gmail.com>
2017-09-19 15:25:31 +0800

    部分header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,6 +17,10 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
     <div class="bulletin-wrapper"></div>
 	</div>
@@ -39,8 +43,9 @@
   @import "../../common/stylus/mixin.styl"
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -92,7 +97,26 @@
               bg-image('special_1')
           .text
             line-height: 12px
-            font-size: 12px
+            font-size: 10px
+
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background-color: rgba(0,0,0,0.2)
+        text-align: center
+        .count
+          font-size: 10px
+          vertical-align: top
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
 
 
 </style>
