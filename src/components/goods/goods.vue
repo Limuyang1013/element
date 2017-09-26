64e0bf6d66d8d2ce1df56da7d4e8fdfcbd604eb7
Limuyang <lin794653318@gmail.com>
2017-09-26 17:59:18 +0800

    实现购物车滑动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -9,7 +9,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
           <h1 class="title">{{item.name}}</h1>
@@ -22,12 +22,12 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好
+                  评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">¥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span><span
+                  class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +39,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
 const ERR_OK = 0
 export default {
   props: {
@@ -57,9 +58,17 @@ export default {
       response = response.body
       if (response.errno === ERR_OK) {
         this.goods = response.data
-        console.log(this.goods)
+        this.$nextTick(() => {
+          this._initScroll()
+        })
       }
     })
+  },
+  methods: {
+    _initScroll() {
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {})
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+    }
   }
 }
 </script>
@@ -142,6 +151,7 @@ export default {
             font-size: 10px
             color: rgb(147,153,159)
           .desc
+            line-height: 12px
             margin-bottom: 8px
           .extra
             .count
