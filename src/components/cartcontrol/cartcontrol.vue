fa057562ece81ba14b2b852192958e323e61bc1f
Limuyang <lin794653318@gmail.com>
2017-10-19 15:26:02 +0800

    8-5 finished

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="cartcontrol">
       <transition name="fade">
-        <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart">
           <transition name="inner">
             <span class="inner icon-remove_circle_outline"></span>
           </transition>
         </div>
       </transition>
       <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
-      <div class="cart-add icon-add_circle" @click="addCart"></div>
+      <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
     </div>
 </template>
 
--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,16 +1,38 @@
 <template>
   <transition name="fade">
-    <div v-show="showFlag" class="food">
+    <div v-show="showFlag" class="food" ref="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image" alt="">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
         </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <transition name="fade">
+          <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst">加入购物车</div>
+        </transition>
       </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import Vue from 'vue'
+  import cartcontrol from '../cartcontrol/cartcontrol.vue'
   export default {
     props: {
       food: {
@@ -25,7 +47,28 @@
     methods: {
       show() {
          this.showFlag = true
+         this.$nextTick(() => {
+           if (!this.scroll) {
+             this.scroll = new BScroll(this.$refs.food, {
+               click: true
+             })
+           } else {
+             this.scroll.refresh()
+           }
+         })
+      },
+      hide() {
+        this.showFlag = false
+      },
+      addFirst(event) {
+        if (!event._constructed) {
+          return
+        }
+        Vue.set(this.food, 'count', 1)
       }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
@@ -58,4 +101,70 @@
         left: 0
         width: 100%
         height: 100%
+        object-fit: cover
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #fff
+
+
+    .content
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7,17,27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147,153,159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240,20,20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147,153,159)
+
+    .cartcontrol-wrapper
+      position: absolute
+      right: 12px
+      bottom: 12px
+    .buy
+      position: absolute
+      right: 18px
+      bottom: 18px
+      z-index: 10
+      height: 24px
+      line-height: 24px
+      padding: 0 12px
+      box-sizing: border-box
+      border-radius: 12px
+      font-size: 10px
+      color: #fff
+      background: rgb(0,160,220)
+      &.fade-enter-active, &.fade-leave-active {
+        transition: all 0.2s
+        opacity: 0
+      }
+      &.fade-enter, &.fade-leave-active {
+        opacity: 0
+      }
 </style>
