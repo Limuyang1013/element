b42115b963298f95be99bf708b971d3b53ee4edd
Limuyang <lin794653318@gmail.com>
2017-10-31 10:59:20 +0800

    修复部分显示问题

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -12,19 +12,19 @@
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>
+              <span class="stress">{{seller.minPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>商家配送</h2>
             <div class="content">
-              <span class="stress">{{seller.deliveryPrice}}</span>
+              <span class="stress">{{seller.deliveryPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>平均配送时间</h2>
             <div class="content">
-              <span class="stress">{{seller.deliveryTime}}</span>
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
             </div>
           </li>
         </ul>
